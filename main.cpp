36a54d2c882a79d9dfd61dc61ec927e0b1e76de1
haveyouwantto <haveyouwantto@gmail.com>
2019-08-25 19:40:10 +0800

    L10

--- main.cpp
+++ main.cpp
@@ -32,6 +32,33 @@ double pascal(int x,int y){
   }
 }
 
+bool isLeap(int year){
+    if(year%4==0&&year%100!=0||year%400==0){
+        return true;
+    }else return false;
+}
+
+int days(int year, int month){
+    switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        return 30;
+    case 2:
+        return 28+isLeap(year);
+    }
+    return 0;
+}
+
 int main(){
 
   for(int i =1;i<20;++i){
